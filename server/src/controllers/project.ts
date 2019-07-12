5fd71d981e4156aff2c6fb50c5adb0a603c25cbc
Jan WALSH <jwalsh@e1r9p7.42.fr>
2019-07-12 16:53:42 +0200

    Check for existing project

--- server/src/controllers/project.ts
+++ server/src/controllers/project.ts
@@ -2,14 +2,22 @@ import { Project } from '../models/project'
 import { User, IUser } from '../models/user'
 import { Request, Response } from 'express';
 import chalk from 'chalk';
+import ResponseStatusTypes from '../utils/ResponseStatusTypes';
 
 export class ProjectController {
 	public async create(req: Request, res: Response) {
 		console.log(chalk.magenta('[ProjectContoller] create'), req.body);
 		const { name, user } = req.body; // User acquired from authorization middleware
 	   
+		const existingProject = await Project.findOne({name});
+		if (existingProject) {
+			console.log(chalk.red(`Project with name ${name} already exists`));
+			res.statusCode = ResponseStatusTypes.BAD_REQUEST;
+			res.send({err: `Project with name ${name} already exists`});
+			return ;
+		}
 		const newProject = new Project({name, owner: user._id});
-    
+
         newProject.save((err, project) => {
             if (err){
                 res.send(err);
