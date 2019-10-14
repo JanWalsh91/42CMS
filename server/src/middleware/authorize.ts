a8f47f0e25a54963036214e16d4e41a3d688e398
Jan WALSH <jwalsh@e2r9p8.42.fr>
2019-10-14 10:49:22 +0200

    cleaned up middleware

--- server/src/middleware/authorize.ts
+++ server/src/middleware/authorize.ts
@@ -1,5 +1,3 @@
-import chalk from 'chalk'
-
 import { Response, Request, NextFunction } from 'express'
 import { User } from '../models'
 import ResponseStatusTypes from '../utils/ResponseStatusTypes'
@@ -11,30 +9,26 @@ import ResponseStatusTypes from '../utils/ResponseStatusTypes'
  * Ends request if not authorized with error msg.
  */
 export default async (req: Request, res: Response, next: NextFunction) => {
-	console.log(chalk.magenta('AUTHORIZING'));
 	if (!req || !req.hasOwnProperty('session')) {
-		return notAuthorized('No session');
+		return notAuthorized('No session')
 	}
 	if (req.session.hasOwnProperty('apiKey')) {
-		// console.log(chalk.green('got api key! =>', req.session.apiKey));
 		User.findOne({apiKey: req.session.apiKey}, (err, user) => {
 			if (err) {
 				return notAuthorized('User not found. Error:' + err)
 			} else if (user) {
-				console.log(chalk.green('Found user! =>', user.username));
-				res.locals.user = user;
-				next();
+				res.locals.user = user
+				next()
 			} else {
 				return notAuthorized('User not found. ' + err)
 			}
-		});
+		})
 	} else {
-		return notAuthorized('No apiKey');
+		return notAuthorized('No apiKey')
 	}
 
 	function notAuthorized(errMsg: string) {
-		console.log(chalk.red('Not authorized:', errMsg))
-		res.statusCode = ResponseStatusTypes.UNAUTHORIZED;
-		res.send({error: 'Unauthorized'});
+		res.statusCode = ResponseStatusTypes.UNAUTHORIZED
+		res.send({error: errMsg})
 	}
 }
