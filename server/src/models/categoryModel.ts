c264e1d90bdeab4d5e0d15a2e9eb0eb089ee7c5c
Jan WALSH <jwalsh@e2r9p8.42.fr>
2019-10-14 10:50:01 +0200

    cleaned up modles

--- server/src/models/categoryModel.ts
+++ server/src/models/categoryModel.ts
@@ -3,4 +3,4 @@ import { model, Model } from 'mongoose'
 import { ICategory } from '../interfaces' 
 import { categorySchema } from '../schemas'
 
-export const Category: Model<ICategory> = model<ICategory>('Category', categorySchema);
+export const Category: Model<ICategory> = model<ICategory>('Category', categorySchema)
--- server/src/models/globalSettingsModel.ts
+++ server/src/models/globalSettingsModel.ts
@@ -3,4 +3,4 @@ import { model, Model } from 'mongoose'
 import { IGlobalSettings } from '../interfaces' 
 import { globalSettingsSchema } from '../schemas'
 
-export const GlobalSettings: Model<IGlobalSettings> = model<IGlobalSettings>('GlobalSettings', globalSettingsSchema);
+export const GlobalSettings: Model<IGlobalSettings> = model<IGlobalSettings>('GlobalSettings', globalSettingsSchema)
